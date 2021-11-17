8f649a71a16fb45183cb76032377518ff6b8f703
dixitgarg059 <dixitgarg059@gmail.com>
2021-11-17 16:01:54 +0530

    implemented hash partition join

--- src/executors/join.cpp
+++ src/executors/join.cpp
@@ -164,6 +164,123 @@ namespace
         return;
     }
 
+#define M 10
+
+    void clear_buffer(Page *p, const string &filename)
+    {
+
+        ofstream fout(filename, ios::app);
+        for (auto row : p->rows)
+        {
+            for (int columnCounter = 0; columnCounter < row.size(); columnCounter++)
+            {
+                if (columnCounter != 0)
+                    fout << ",";
+                fout << row[columnCounter];
+            }
+            fout << endl;
+        }
+        fout.close();
+        p->rows.clear();
+    }
+
+    void InsertRecordIntoPage2(Page *p, vector<int> &record, const string &filename)
+    {
+        p->rows.push_back(record);
+        if (p->rows.size() == BLOCK_SIZE)
+        {
+            clear_buffer(p, filename);
+        }
+    }
+    int Hash(int r)
+    {
+        return r % M;
+    }
+
+    void partitionJoin(string name1, string name2, string column1, string column2, string result_relation, Table *final_table)
+    {
+        Table *A = tableCatalogue.getTable(name1);
+        Table *B = tableCatalogue.getTable(name2);
+        int idx1 = A->getColumnIndex(column1);
+        int idx2 = B->getColumnIndex(column2);
+
+        vector<Page *> buffers(M);
+        vector<string> filenameA(M), filenameB(M);
+        for (int i = 0; i < M; i++)
+        {
+            filenameA[i] = "../data/Partition_A" + to_string(i) + ".csv";
+            filenameB[i] = "../data/Partition_B" + to_string(i) + ".csv";
+            ofstream fout(filenameA[i], ios::out);
+            for (const auto &col : A->columns)
+            {
+                fout << col;
+                if (col != A->columns.back())
+                    fout << ",";
+            }
+            fout << endl;
+            fout.close();
+
+            ofstream fout2(filenameB[i], ios::out);
+            for (const auto &col : B->columns)
+            {
+                fout2 << col;
+                if (col != B->columns.back())
+                    fout2 << ",";
+            }
+            fout2 << endl;
+            fout2.close();
+        }
+        for (auto &it : buffers)
+        {
+            it = new Page();
+        }
+        for (int i = 0; i < A->blockCount; i++)
+        {
+            Page *p = bufferManager.getPage(name1, i);
+            for (auto record : p->rows)
+            {
+
+                int bucket_number = Hash(record[idx1]);
+                InsertRecordIntoPage2(buffers[bucket_number], record, filenameA[bucket_number]);
+            }
+        }
+        for (int i = 0; i < M; i++)
+        {
+            clear_buffer(buffers[i], filenameA[i]);
+        }
+
+        for (int i = 0; i < B->blockCount; i++)
+        {
+            Page *p = bufferManager.getPage(name1, i);
+            for (auto record : p->rows)
+            {
+
+                int bucket_number = Hash(record[idx1]);
+                InsertRecordIntoPage2(buffers[bucket_number], record, filenameB[bucket_number]);
+            }
+        }
+        for (int i = 0; i < M; i++)
+        {
+            clear_buffer(buffers[i], filenameB[i]);
+        }
+
+        for (int i = 0; i < M; i++)
+        {
+
+            string a = "Partition_A" + to_string(i);
+            string b = "Partition_B" + to_string(i);
+            cout << a << " " << b << endl;
+            Table *A = new Table(a);
+            Table *B = new Table(b);
+            if (A->load() && B->load())
+            {
+                tableCatalogue.insertTable(A);
+                tableCatalogue.insertTable(B);
+            }
+            Join(a, b, column1, column2, result_relation, final_table);
+        }
+        return;
+    }
 }
 void executeJOIN()
 {
@@ -175,7 +292,7 @@ void executeJOIN()
         result_columns.push_back(it);
     Table *final_table = new Table(parsedQuery.joinResultRelationName, result_columns);
     tableCatalogue.insertTable(final_table);
-    Join(parsedQuery.joinFirstRelationName, parsedQuery.joinSecondRelationName, parsedQuery.joinFirstColumnName, parsedQuery.joinSecondColumnName, parsedQuery.joinResultRelationName, final_table);
+    partitionJoin(parsedQuery.joinFirstRelationName, parsedQuery.joinSecondRelationName, parsedQuery.joinFirstColumnName, parsedQuery.joinSecondColumnName, parsedQuery.joinResultRelationName, final_table);
     cout << "joined table";
     logger.log("executeJOIN");
     return;
